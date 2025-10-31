fabc799afb1b43e663fa4b704c9681f83f6fb39b
vishalamc <vishalamc@gmail.com>
2025-11-01 00:33:58 +0530

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,23 @@ import "../App.css";
 import { FcBullish } from "react-icons/fc";
 
 const learnCourses = [
-  { icon: <FcBullish className="course-icon" />, title: "Computer Courses", desc: "Smart Teaching • Regular Tests", link: "/ComputerCourse" },
-  { icon: <FcBullish className="course-icon" />, title: "BCA", desc: "Smart Teaching • Regular Tests", link: "/ComingSoon" },
-  { icon: <FcBullish className="course-icon" />, title: "MCA", desc: "Smart Teaching • Regular Tests", link: "/ComingSoon" },
-  { icon: <FcBullish className="course-icon" />, title: "BTech(CSE)", desc: "Smart Teaching • Regular Tests", link: "/ComingSoon" }
+  { icon: <FcBullish className="course-icon" />, title: "Web Development", desc: "Smart Teaching • Regular Tests", link: "/comingsoon" },
+  { icon: <FcBullish className="course-icon" />, title: "BCA", desc: "Smart Teaching • Regular Tests", link: "/comingsoon" },
+  { icon: <FcBullish className="course-icon" />, title: "MCA", desc: "Smart Teaching • Regular Tests", link: "/comingsoon" },
+  { icon: <FcBullish className="course-icon" />, title: "BTech(CSE)", desc: "Smart Teaching • Regular Tests", link: "/comingsoon" }
 ];
 
 const practiceCourses = [
-  { title: "Computer Courses", img: "/computer.png", link: "/ComingSoon" },
-  { title: "BCA", img: "/BCA.png", link: "/ComingSoon" },
-  { title: "MCA", img: "/MCA.png", link: "/ComingSoon" },
-  { title: "BTech(CSE)", img: "/btech.png", link: "/ComingSoon" },
+  { title: "Computer Courses", img: "/computer.png", link: "/computercourse" },
+  { title: "BCA Class", img: "/BCA.png", link: "/bcacourse" },
+  { title: "MCA Class", img: "/MCA.png", link: "/mcacourse" },
+  { title: "BTech(CSE) Class", img: "/btech.png", link: "/btechcourse" },
+];
+const technologySolutions = [
+  { title: "Website or Web App", img: "/website.jpg", link: "/comingsoon" },
+  { title: "Mobile App", img: "/mobile.jpg", link: "/ComingSoon" },
+  { title: "Accounting Software", img: "/account.jpg", link: "/ComingSoon" },
+  { title: "Stock Management", img: "/stock.jpg", link: "/ComingSoon" },
 ];
 
 export default function Home() {
@@ -35,28 +41,28 @@ export default function Home() {
         <a href="#learn-section" className="top-btn learn-top">Learn</a>
         <a href="#practice-section" className="top-btn practice-top">Practice</a>
       </div>
-
-      {/* Learn Section */}
-      <h2 id="learn-section" className="sectin-title">Course Details</h2>
-      <div className="cards-container">
-        {learnCourses.map((course, i) => (
-          <div className="card" key={i}>
-            <div className="icon">{course.icon}</div>
-            <h2>{course.title}</h2>
-            <p>{course.desc}</p>
-            <Link to={course.link} className="learn-btn">Details</Link>
-          </div>
-        ))}
-      </div>
+ 
 
       {/* Practice Section */}
-      <h2 id="practice-section" className="section-title">Study Materials</h2>
+      <h2 id="practice-section" className="section-title">Future Lines-Academy</h2>
       <div className="cards-container">
         {practiceCourses.map((course, i) => (
           <div className="card" key={i}>
             <img src={course.img} alt={course.title} className="card-img" />
             <h2>{course.title}</h2>
-            <Link to={course.link} className="learn-btn">Reading Materials</Link>
+            <Link to={course.link} className="learn-btn">Course Details</Link>
+          </div>
+        ))}
+      </div>
+
+      {/* Learn Section */}
+      <h2 id="learn-section" className="sectin-title">Future Lines-Technology Solutions</h2>
+      <div className="cards-container">
+        {technologySolutions.map((course, i) => (
+          <div className="card" key={i}>
+            <img src={course.img} alt={course.title} className="card-img" />
+            <h2>{course.title}</h2>
+            <Link to={course.link} className="learn-btn">Go for Details</Link>
           </div>
         ))}
       </div>
